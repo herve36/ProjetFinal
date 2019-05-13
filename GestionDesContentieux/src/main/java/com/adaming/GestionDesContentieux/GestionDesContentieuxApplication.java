62f76d9831726ba1ce8e5f3a8d7d086c97883131
herve36 <romain.v@orange.fr>
2019-05-13 10:32:25 +0200

    Porjet Final

---
+++ GestionDesContentieux/src/main/java/com/adaming/GestionDesContentieux/GestionDesContentieuxApplication.java
@@ -0,0 +1,13 @@
+package com.adaming.GestionDesContentieux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesContentieuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesContentieuxApplication.class, args);
+	}
+
+}
